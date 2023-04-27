2d4420160703c34d5c2fc8bac95b1b4b447a6402
PARKEUNGYEONG <eungyeong1140@naver.com>
2023-04-27 12:14:06 +0900

    입출력을 응용하는 문제??!

---
+++ BaekJoon/src/입출력과사칙연산/Login.java
@@ -0,0 +1,22 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// 준하는 사이트에 회원가입을 하다가 joonas라는 아이디가 이미 존재하는 것을 보고 놀랐다. 준하는 놀람을 ??!로 표현한다. 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어졌을 때, 놀람을 표현하는 프로그램을 작성하시오.
+		 Scanner sc = new Scanner(System.in);
+		 
+		 String id = sc.nextLine();
+		
+		 if(id.equals("joonas")) {
+			  System.out.println(id+"?!!");
+		 }else {
+			  System.out.println("사용 가능한 아이디입니");
+		 }
+		 
+		 
+		 sc.close();	
+	}
+}
