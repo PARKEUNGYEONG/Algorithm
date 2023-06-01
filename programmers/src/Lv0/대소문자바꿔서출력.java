0d966571df514095814edad950a736d72afc7db7
PARKEUNGYEONG <eungyeong1140@naver.com>
2023-06-01 23:54:35 +0900

    대소문자바꿔서출력

---
+++ programmers/src/Lv0/대소문자바꿔서출력.java
@@ -0,0 +1,17 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class 대소문자바꿔서출력 {
+
+	public static void main(String[] args) {
+		// 문제 : 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+		//toLowerCase();/toUpperCase(); - 이 두 메서드는 소문자/대문자로 변환해준다.
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+
+	}
+
+}
